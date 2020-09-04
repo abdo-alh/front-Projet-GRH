a7590068a3437fd3f1932d2f60fea0c8fc4c28b9
abdoal <abdrahimalhyane5@gmail.com>
2020-09-04 15:22:19 +0100

    abderrahim alhyane commit fix attestation

--- src/app/components/attestation/attestation-employee/attestation-employee.component.ts
+++ src/app/components/attestation/attestation-employee/attestation-employee.component.ts
@@ -1,11 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ModalDismissReasons, NgbModal, NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import pdfMake from 'pdfmake/build/pdfmake';
-import pdfFonts from 'pdfmake/build/vfs_fonts';
 import { Employee } from '../../../controller/model/employee';
 import { Stage } from '../../../controller/model/stage';
 import { EmployeeService } from '../../../controller/service/employee.service';
-pdfMake.vfs = pdfFonts.pdfMake.vfs;
+
 
 @Component({
   selector: 'app-attestation-employee',
@@ -21,29 +18,8 @@ export class AttestationEmployeeComponent implements OnInit {
     this.getAllEmployees();
   }
 
-  title = 'appBootstrap';
+  constructor(private employeeService: EmployeeService) { }
 
-  closeResult: string;
-
-  constructor(private modalService: NgbModal, private employeeService: EmployeeService) { }
-
-  open(content) {
-    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
-    }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-    });
-  }
-
-  private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    } else {
-      return `with: ${reason}`;
-    }
-  }
 
   public getAllEmployees() {
     this.employeeService.getAllEmployees().subscribe(
@@ -60,123 +36,6 @@ export class AttestationEmployeeComponent implements OnInit {
     console.log(value);
   }
 
-  generatePdf() {
-    const documentDefinition = { content: 'This is an sample PDF printed with pdfMake' };
-    pdfMake.createPdf(documentDefinition).open();
-  }
-
-  downloadPdf() {
-    const documentDefinition = { content: 'This is an sample PDF printed with pdfMake' };
-    pdfMake.createPdf(documentDefinition).download();
-  }
-
-  generatorPdf(action = 'open') {
-    const documentDefinition = this.getDocumentDefinition();
-    switch (action) {
-      case 'open': pdfMake.createPdf(documentDefinition).open();
-        break;
-      case 'print': pdfMake.createPdf(documentDefinition).print();
-        break;
-      case 'download':
-        pdfMake.createPdf(documentDefinition).download();
-        break;
-      default: pdfMake.createPdf(documentDefinition).open();
-        break;
-    }
-
-
-  }
-
-  getDocumentDefinition() {
-    return {
-      content: [
-        {
-          text: 'MARRAKECH LE 12/07/2020',
-          margin: [350, 0, 0, 0],
-          bold: true
-        },
-        {
-          text: 'ATTESTATION DE TRAVAIL',
-          bold: true,
-          fontSize: 20,
-          alignment: 'center',
-          margin: [0, 150, 0, 100]
-        },
-        {
-          text: [
-            {
-              text: [
-                {
-                  text: "Je, soussigné M.RACHID BENNIS , agissant en qualité de gérant de l'entreprise "
-                },
-                {
-                  text: "CoreUi ",
-                  color: "red"
-                },
-                {
-                  text: ", atteste que ",
-                },
-                {
-                  text: 'M. ' + this.employee.nom + ' ' + this.employee.prenom,
-                  bold: true,
-                  style: ['html-strong']
-                },
-                {
-                  text: ', porteur de la CIN N° : '
-                },
-                {
-                  text: this.employee.adresse,
-                  italics: true,
-                  style: ['html-em']
-                },
-                {
-                  text: ' travaille au sein de notre société depuis le '
-                },
-                {
-                  text: '1 Mars 2020 . en qualité de Developpeur .'
-                }
-              ],
-              style: ['html-p']
-            }
-          ],
-        },
-        {
-          text: [
-            {
-              text: 'M. ' + this.employee.nom + ' ' + this.employee.prenom,
-              bold: true,
-              style: ['html-strong']
-            },
-            {
-              text: ' est sous contrat à durée indéterminée (CDI) depuis la date mentionnée ci-dessus et titularisé depuis le 1er Décembre 2013.'
-            }
-          ],
-          margin: [0, 30, 0, 0],
-          style: ['html-p']
-        },
-        {
-          text: 'M. RACHID BENNIS',
-          alignment: 'right',
-          bold: true,
-          margin: [0, 170, 0, 0],
-          style: ['html-strong']
-        }
-      ],
-      styles: {
-        name: {
-          fontSize: 16,
-          bold: true
-        }
-      }
-    };
-  }
-
-  /*
-  Download the PDF : pdfMake.createPdf(docDefinition).download();
-  Open the PDF in new window : pdfMake.createPdf(docDefinition).open(); 
-  Open PDF in same window : pdfMake.createPdf(docDefinition).open({}, window);
-  Print the PDF: pdfMake.createPdf(docDefinition).print();
-  */
 
 
 }
--- src/app/components/attestation/attestation-stagiaire/attestation-stagiaire.component.ts
+++ src/app/components/attestation/attestation-stagiaire/attestation-stagiaire.component.ts
@@ -1,11 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Stagiaire } from '../../../controller/model/stagiaire';
-import pdfMake from 'pdfmake/build/pdfmake';
-import pdfFonts from 'pdfmake/build/vfs_fonts';
 import { EquipeItem } from '../../../controller/model/equipe-item';
 import { StagiaireService } from '../../../controller/service/stagiaire.service';
-pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
   selector: 'app-attestation-stagiaire',
@@ -18,28 +14,10 @@ export class AttestationStagiaireComponent implements OnInit {
   equipeItem: EquipeItem;
   stagiaires:Stagiaire[];
 
-  constructor(private modalService: NgbModal,private stagiaireService:StagiaireService) { }
+  constructor(private stagiaireService:StagiaireService) { }
 
   ngOnInit(): void {
     
-  }
-
-  open(content) {
-    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
-    }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-    });
-  }
-
-  private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    } else {
-      return `with: ${reason}`;
-    }
   }
 
   public getAllStagiaires(){
@@ -55,112 +33,6 @@ export class AttestationStagiaireComponent implements OnInit {
   
   public onOptionsSelected(value: Stagiaire){
     console.log(value);
-  }
-
-
-  generatePdf() {
-    const documentDefinition = { content: 'This is an sample PDF printed with pdfMake' };
-    pdfMake.createPdf(documentDefinition).open();
-  }
-
-  downloadPdf() {
-    const documentDefinition = { content: 'This is an sample PDF printed with pdfMake' };
-    pdfMake.createPdf(documentDefinition).download();
-  }
-
-  generatorPdf(action = 'open') {
-    const documentDefinition = this.getDocumentDefinition();
-    switch (action) {
-      case 'open': pdfMake.createPdf(documentDefinition).open();
-        break;
-      case 'print': pdfMake.createPdf(documentDefinition).print();
-        break;
-      case 'download':
-        pdfMake.createPdf(documentDefinition).download();
-        break;
-      default: pdfMake.createPdf(documentDefinition).open();
-        break;
-    }
-
-
-  }
-
-  getDocumentDefinition() {
-    return {
-      content: [
-        {
-          text: 'MARRAKECH LE 12/07/2020',
-          margin: [350, 0, 0, 0],
-          bold: true
-        },
-        {
-          text: 'ATTESTATION DE STAGE',
-          bold: true,
-          fontSize: 20,
-          alignment: 'center',
-          margin: [0, 150, 0, 100]
-        },
-        {
-          text: [
-            {
-              text: [
-                {
-                  text: "Je, soussigné "
-                },
-                {
-                  text: 'M. ' + this.equipeItem.stage.encadrent.nom + ' ' + this.equipeItem.stage.encadrent.prenom,
-                  bold: true,
-                  style: ['html-strong']
-                },
-                {
-                  text: ' au sein de l’entreprise '
-                },
-                {
-                  text: "KechTrip ",
-                  color: "red"
-                },
-                {
-                  text: "sise Résidence Attrani - Avenue Hassan II - Guéliz 40 010 Marrakech atteste par la présente que ",
-                },
-                {
-                  text: 'M. ' + this.equipeItem.stagiaire.nom + ' ' + this.equipeItem.stagiaire.prenom,
-                  bold: true,
-                  style: ['html-strong']
-                },
-                {
-                  text: ' a réalisé un stage dans le département Informatique de notre entreprise du 03/09/2019 au 30/09/2019.'
-                },
-                {
-                  text: " Durant son stage, il a montré capacités évidentes d’initiatives et d'adaptation aux différents types de tâches qui lui ont été demandées d'assurer. "
-                }
-              ],
-              style: ['html-p']
-            }
-          ],
-        },
-        {
-          text: [
-            {
-              text: 'Cette attestation est délivrée à l’intéressé pour valoir ce que de droit.'
-            }
-          ],
-          margin: [0, 30, 0, 0],
-          style: ['html-p']
-        },
-        {
-          text: 'M. ' + this.equipeItem.stage.encadrent.nom + ' ' + this.equipeItem.stage.encadrent.prenom,
-          bold: true,
-          style: ['html-strong'],
-          margin: [0, 170, 0, 0]
-        }
-      ],
-      styles: {
-        name: {
-          fontSize: 16,
-          bold: true
-        }
-      }
-    };
   }
 
 }
